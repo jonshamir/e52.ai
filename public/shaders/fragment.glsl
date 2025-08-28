a9e2aac7f962d5db9c70ebb0c626b9f16107fbed
Jon Shamir <jon.shamir@gmail.com>
2025-08-29 00:11:09 +0300

    remove dist

--- public/shaders/fragment.glsl
+++ public/shaders/fragment.glsl
@@ -63,7 +63,7 @@ void main() {
     float aaNoise = fwidth(minD);
 
     float mask = (1.0 - smoothstep(0.0, aaNoise, minD));
-    float distanceFromCenter = 1.0 - length(p * 3.0);
+    // float distanceFromCenter = 1.0 - length(p * 3.0);
 
-    gl_FragColor = vec4(0.369, 0.365, 0.357, clamp(mask, 0.0, 1.0)) * distanceFromCenter * 0.5;
+    gl_FragColor = vec4(0.369, 0.365, 0.357, clamp(mask, 0.0, 1.0)) * 0.5;
 }
