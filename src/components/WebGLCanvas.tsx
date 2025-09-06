a67c9921cbb3b2cd35a9e27274fefdba6679a589
Jon Shamir <jon.shamir@gmail.com>
2025-09-06 21:10:21 +0300

    fill

--- src/components/WebGLCanvas.tsx
+++ src/components/WebGLCanvas.tsx
@@ -146,9 +146,10 @@ export default function WebGLCanvas() {
     <Canvas
       orthographic
       camera={{ position: [0, 0, 5], zoom: 1 }}
+      dpr={[1, 2]}
       gl={{ alpha: true, premultipliedAlpha: false }}
       className="effect-canvas"
-      style={{ width: "100%", height: "100%" }}
+      style={{ position: "fixed", inset: 0, width: "100vw", height: "100vh" }}
     >
       <Quads count={QUAD_COUNT} />
     </Canvas>
