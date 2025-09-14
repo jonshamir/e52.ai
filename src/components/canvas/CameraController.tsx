5922f4237a197dbc1ed5a0716f9d3c26efa5365a
Jon Shamir <jon.shamir@gmail.com>
2025-09-14 09:51:15 +0300

    fix

--- src/components/canvas/CameraController.tsx
+++ src/components/canvas/CameraController.tsx
@@ -2,7 +2,6 @@
 
 import { useRef, useEffect } from "react";
 import { useThree } from "@react-three/fiber";
-import * as THREE from "three";
 import { easeInOutCubic } from "../../utils/easing";
 
 export default function CameraController() {
