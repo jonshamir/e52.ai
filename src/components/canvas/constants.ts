03045e3cdcb4195d04f22adca99c88733fa38e73
Jon Shamir <jon.shamir@gmail.com>
2025-09-29 17:51:08 +0900

    blue version

--- src/components/canvas/constants.ts
+++ src/components/canvas/constants.ts
@@ -2,8 +2,8 @@ import * as THREE from "three";
 
 export const QUAD_COUNT = 20; // n points
 export const POINT_RADIUS = 6; // radius in pixels
-export const POINT_COLOR = new THREE.Color(0.387, 0.519, 0.824);
+export const POINT_COLOR = new THREE.Color(0.838, 0.903, 0.986);
 
 export const LINE_COUNT = 35; // n lines
 export const LINE_WIDTH = 1.2; // line thickness in pixels
-export const LINE_COLOR = new THREE.Color(0.838, 0.903, 0.986);
+export const LINE_COLOR = new THREE.Color(0.387, 0.519, 0.824);
